74db65cf5e87b9bd979266b57a6f55349e4e4d96
Damjan Pavlovic <pavlovic.damjan1828@gmail.com>
2025-08-21 13:21:03 +0200

    Added frontend and backend to the project

---
+++ react-project/src/components/PostsList.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import NewPost from "./NewPost";
+import Post from "./Post";
+import classes from "./PostList.module.css";
+import Modal from "./Modal"
+
+
+
+function PostsList({isPosting, onStopPosting}) {
+  const [posts, setPosts] = useState([])
+
+  function addPostHandler(postData) {
+    fetch('http://localhost:8080/posts', {
+      method: 'POST',
+      body: JSON.stringify(postData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    setPosts((existingPosts) => [postData, ...existingPosts]);
+  }
+  return (
+    <>
+      {isPosting &&(
+      <Modal onClose={onStopPosting}>
+        <NewPost 
+        onCancel={onStopPosting}
+        onAddPost={addPostHandler}
+        />
+      </Modal>
+    )}
+      {posts.length > 0 && (
+      <ul className={classes.posts}>
+        {posts.map((post) => <Post key={post.body} author={post.author} body={post.body}/>)}
+      </ul>
+      )}
+      {posts.length === 0 && ( 
+        <div style={{textAlign: 'center', color: 'White'}}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some!</p>
+        </div>)
+        }
+    </>
+  );
+}
+
+export default PostsList;
